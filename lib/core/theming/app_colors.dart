bc99ed4590f910c845f504168ee81d74996c299a
20192726 <20192726@fue.edu.eg>
2024-09-28 22:40:02 +0300

    Calendar added to the screen

--- lib/core/theming/app_colors.dart
+++ lib/core/theming/app_colors.dart
@@ -6,6 +6,8 @@ class AppColors {
   static const greyColor = Color(0xFFF4F5F5);
   static const blueColor = Color(0xFF3498DB);
   static const greyTextColor = Color(0xFF4D4D4D);
+  static const greyColor2 = Color(0xFFF2F2F2);
+  static const greyColor3 = Color(0xFFCCCCCC);
   static const blue2 = Color(0xFFF2F5F8);
   static const blue3 = Color(0xFFCFD9E6);
   static const blue4 = Color(0xFFF3F5F9);
--- lib/core/widgets/primary_app_bar.dart
+++ lib/core/widgets/primary_app_bar.dart
@@ -3,15 +3,31 @@ import 'package:workspace/core/theming/styles.dart';
 
 class PrimaryAppBar extends StatelessWidget {
   final String title;
-  const PrimaryAppBar({super.key, required this.title});
+  final bool isTitleCentered;
+  final bool hasLeadingIcon;
+
+  const PrimaryAppBar({
+    super.key,
+    required this.title,
+    this.isTitleCentered = true,
+    this.hasLeadingIcon = false,
+  });
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       elevation: 2.2,
       backgroundColor: Colors.white,
-      title: Text(title,style:TextStyles.font18BlackSemiBold),
-      centerTitle: true,
+      title: Text(title, style: TextStyles.font18BlackSemiBold),
+      centerTitle: isTitleCentered,
+      leading: hasLeadingIcon
+          ? IconButton(
+        icon: const Icon(Icons.arrow_back, color: Colors.black),
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+      )
+          : null,
     );
   }
 }
